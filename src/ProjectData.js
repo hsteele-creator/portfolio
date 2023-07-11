8f91e436e58539e45f7e9dd670a0376d90e455cc
Harry Steele <harrybsteele@gmail.com>
2023-07-11 13:22:32 -0400

    added project data

---
+++ src/ProjectData.js
@@ -0,0 +1,66 @@
+const ProjectData = [
+    {
+        image : "",
+        name : "Credit Card Dashboard",
+        description : "",
+        skills : ["Javascript", "React", "Redux"],
+        link : "https://credit-card-dashboard.vercel.app",
+    },
+    {
+        image : "",
+        name : "Shopping Cart",
+        description : "",
+        skills : ["Javascript", "React", "Redux"],
+        link : "https://react-grocery-store-ten.vercel.app",
+    },
+    {
+        image : "",
+        name : "Todo List",
+        description : "",
+        skills : ["Javascript", "React", "Redux"],
+        link : "https://redux-todo-list-iota.vercel.app",
+    },
+    {
+        image : "",
+        name : "Meme Generator",
+        description : "",
+        skills : ["Javascript", "React"],
+        link : "https://react-redux-meme-generator.vercel.app",
+    },
+    {
+        image : "",
+        name : "Interactive Rating Component",
+        description : "",
+        skills : ["HTML", "CSS", "Javascript"],
+        link : "https://hsteele-creator.github.io/interactive-rating-component/",
+    },
+    {
+        image : "",
+        name : "Interactive Notifications Page",
+        description : "",
+        skills :  ["HTML", "CSS", "Javascript"],
+        link : "https://hsteele-creator.github.io/notifications-page/",
+    },
+    {
+        image : "",
+        name : "Memory Game",
+        description : "",
+        skills :  ["HTML", "CSS", "Javascript"],
+        link : "https://hsteele-creator.github.io/memoryGame/",
+    },
+    {
+        image : "",
+        name : "Tic Tac Toe",
+        description : "",
+        skills :  ["HTML", "CSS", "Javascript"],
+        link : "https://hsteele-creator.github.io/tic-tac-toe/",
+    },
+    {
+        image : "",
+        name : "Connect Four",
+        description : "",
+        skills :  ["HTML", "CSS", "Javascript"],
+        link : "https://hsteele-creator.github.io/connect-four-oo/",
+    }
+
+]
