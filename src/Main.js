f428692e005c78d437d891dbb88967726a58b306
Harry Steele <harrybsteele@gmail.com>
2023-07-12 14:31:23 -0400

    updated styling for main text

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Main.css";
-import navy from "./Images/navy.jpg"
+import navy from "./Images/navy.jpg";
 
 const Main = () => {
   return (
@@ -14,8 +14,14 @@ const Main = () => {
           backgroundRepeat: "no-repeat",
         }}
       >
-        <h1 id="main-heading">Hi, My Name is Harry Steele</h1>
-        <p id="main-text">Frontend developer, specializing in React and Redux.</p>
+        <div id="main-text">
+          <h1 id="main-heading">Hi, My Name is Harry Steele</h1>
+          <p id="main-text">
+            I am a frontend developer, specializing in React and Redux.
+          </p>
+          <button id="main-btn">View My Projects</button>
+        </div>
+        <div id="main-other"></div>
       </div>
     </>
   );
